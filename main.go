08a936de1fe7f334412656d239db7242bdfc1b40
Michał Korman <michal.korman@icloud.com>
2022-08-25 23:50:49 +0200

    add bidirectional streaming

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ import (
 	"github.com/mkorman9/go-commons/lifecycle"
 	"github.com/mkorman9/go-commons/logutil"
 	"github.com/mkorman9/go-grpc-example/protocol"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"strings"
 )
+
+const correctToken = "secret"
 
 type GreeterService struct {
 }
 
 func (gs *GreeterService) SayHello(ctx context.Context, request *protocol.HelloRequest) (*protocol.HelloReply, error) {
 	token := grpcutil.GetToken(ctx)
-	if token != "secret" {
+	if token != correctToken {
 		return nil, status.Error(codes.Unauthenticated, "invalid token")
 	}
 
@@ -28,12 +32,45 @@ func (gs *GreeterService) SayHello(ctx context.Context, request *protocol.HelloR
 	return response, nil
 }
 
+type GameService struct {
+}
+
+func (gs *GameService) Play(stream protocol.GameService_PlayServer) error {
+	token := grpcutil.GetToken(stream.Context())
+	if token != correctToken {
+		return status.Error(codes.Unauthenticated, "invalid token")
+	}
+
+	for {
+		request, err := stream.Recv()
+		if err != nil {
+			log.Error().Err(err).Msg("Error while receiving player request")
+			return status.Errorf(codes.Internal, "internal server error")
+		}
+
+		if strings.EqualFold(request.Message, "exit") {
+			_ = stream.Send(&protocol.ServerResponse{
+				Message: "SERVERMSG: Closing",
+			})
+
+			break
+		}
+
+		_ = stream.Send(&protocol.ServerResponse{
+			Message: fmt.Sprintf("SERVERMSG: Received %s", request.Message),
+		})
+	}
+
+	return nil
+}
+
 func main() {
 	configutil.LoadConfig()
 	logutil.SetupLogger()
 
 	server := grpcutil.NewServer()
 	protocol.RegisterGreeterServer(server.Server, &GreeterService{})
+	protocol.RegisterGameServiceServer(server.Server, &GameService{})
 
 	lifecycle.StartAndBlock(server)
 }
