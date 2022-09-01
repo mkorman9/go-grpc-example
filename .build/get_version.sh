c1e92cacb109982b0c3e14c3eb4e085d6e47e45a
Michał Korman <michal.korman@icloud.com>
2022-09-01 19:19:10 +0200

    debug

--- .build/get_version.sh
+++ .build/get_version.sh
@@ -6,12 +6,14 @@ commit_id="$(git rev-parse HEAD)"
 tag_name="$(git describe --contains $commit_id 2> /dev/null)"
 branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-if [[ ! -z "$tag_name" ]]; then
+if [[ -n "$tag_name" ]]; then
   echo "$tag_name"
   exit 0
 fi
 
 last_version="$(git tag --sort=-v:refname | head -1)"
+
+echo "--- $last_version"
 
 if [[ -z "$last_version" ]]; then
   last_version="v0.0.0"
